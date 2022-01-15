dcedeb8162c3cf26ba514a4c95eb8cda213001d0
wojiazaiyugang <wojiazaiyugang@outlook.com>
2022-01-15 23:32:47 +0800

    修改相机SDK新增授权使用功能

--- camera/__init__.py
+++ camera/__init__.py
@@ -1,6 +1,9 @@
-from camera.hik_vision import HIKCamera
-from infer.encryption import verify_authorization
+from pathlib import Path
 
+from camera.hik_vision import HIKCamera
+from camera.encryption import verify_authorization
+
+ROOT_DIR = Path(__file__).parent.parent.resolve()
 verify_authorization(ROOT_DIR.joinpath("authorization.txt"))
 
 __all__ = [
