7529d8494ef3be8e8b042845998fe3c10fcb9fca
wojiazaiyugang <13061044@buaa.edu.cn>
2022-02-15 16:28:05 +0800

    相机SDK暂时不做授权认证机制

--- camera/__init__.py
+++ camera/__init__.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from camera.hik_vision import HIKCamera
 from camera.encryption import verify_authorization
 
-ROOT_DIR = Path(__file__).parent.parent.resolve()
-verify_authorization(ROOT_DIR.joinpath("authorization.txt"))
+# ROOT_DIR = Path(__file__).parent.parent.resolve()
+# verify_authorization(ROOT_DIR.joinpath("authorization.txt"))
 
 __all__ = [
     "HIKCamera"  # 海康相机
