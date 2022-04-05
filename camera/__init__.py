118623d82bf98ca5d7054416779b3ec55a37fd5e
wojiazaiyugang <wojiazaiyugang@outlook.com>
2022-04-05 17:54:08 +0800

    导出Frame数据结构

--- camera/__init__.py
+++ camera/__init__.py
@@ -1,11 +1,11 @@
-from pathlib import Path
-
-from camera.hik_vision import HIKCamera
+from camera.data_class import Frame
 from camera.encryption import verify_authorization
+from camera.hik_vision import HIKCamera
 
 # ROOT_DIR = Path(__file__).parent.parent.resolve()
 # verify_authorization(ROOT_DIR.joinpath("authorization.txt"))
 
 __all__ = [
-    "HIKCamera"  # 海康相机
+    "HIKCamera",  # 海康相机
+    "Frame"  # 视频帧
 ]
--- scripts/test.py
+++ scripts/test.py
@@ -10,16 +10,21 @@ from scripts import get_scripts_output
 
 
 def record():
-    camera = HIKCamera(ip="192.168.111.78", user_name="admin", password="12345678a")
-    camera.start_preview()
-    video = get_scripts_output(f"{datetime.now():%Y%m%d_%H%M%S.mp4}")
-    print(f"开始录像")
-    camera.save_real_data(video)
+    cameras = []
+    videos = []
+    for ip in ["192.168.111.75", "192.168.111.78", "192.168.111.77"]:
+        camera = HIKCamera(ip=ip, user_name="admin", password="12345678a")
+        cameras.append(camera)
+        camera.start_preview()
+        video = get_scripts_output(f"{datetime.now():%Y%m%d_%H%M%S}_{ip}.mp4")
+        camera.save_real_data(video)
+        videos.append(video)
     time.sleep(4)
-    camera.stop_save_real_data()
-    camera.stop_preview()
-    video = cv2.VideoCapture(str(video))
-    print(f"录像结束，视频总帧数{video.get(cv2.CAP_PROP_FRAME_COUNT)}")
+    for camera, video_file in zip(cameras, videos):
+        camera.stop_save_real_data()
+        camera.stop_preview()
+        video = cv2.VideoCapture(str(video_file))
+        print(f"录像结束，视频总帧数{video.get(cv2.CAP_PROP_FRAME_COUNT)}")
 
 
 if __name__ == '__main__':
